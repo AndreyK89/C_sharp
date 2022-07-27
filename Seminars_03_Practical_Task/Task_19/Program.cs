5324764e24efd38d2b36e20983f7ebf301ec50d1
Andrey_K <kaa89job@yandex.ru>
2022-07-27 23:28:46 +0500

    Число полиандр?

---
+++ Seminars_03_Practical_Task/Task_19/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Выполнил Кошелев Андрей/ Группа GU | Разработчик | 2789 |
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+Console.Write("Введите пятизначное число: ");
+int num = int.Parse(Console.ReadLine());
+char[] arr = num.ToString().ToCharArray();
+int lenght = arr.Length;
+
+if (lenght != 5)
+{
+    Console.WriteLine("ERROR: Число " + num + " не пятизначное.");
+}
+else if (arr[0] == arr[4] && arr[1] == arr[3])
+{
+    Console.WriteLine("УРА!!! Число " + num + " -> палиндром");
+}
+else
+{
+    Console.WriteLine("НЕТ: Число " + num + " -> не палиндром");
+}
