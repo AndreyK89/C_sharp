185887a76b9d8fa3e7abf473baebbbe94d05b963
Andrey_K <kaa89job@yandex.ru>
2022-08-03 01:15:27 +0500

    Add Task_26

---
+++ Seminar_4_Functions/Task_26/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Выполнил Кошелев Андрей/ Группа GU | Разработчик | 2789 |
+Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5
+*/
+
+Console.WriteLine(" Введите число: ");
+/* string num = Console.ReadLine() 
+int len = num.Lenght;
+Console.WriteLine(len);
+*/
+int num = int.Parse(Console.ReadLine());
+int i = 0;
+while(num != 0 )
+{
+    num = num / 10;
+    i++;
+    
+}
+Console.Write(i);
