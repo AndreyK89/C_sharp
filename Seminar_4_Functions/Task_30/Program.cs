240547ecc32a6d6377a96adfa61a0e8cdacea5e6
Andrey_K <kaa89job@yandex.ru>
2022-08-03 01:20:41 +0500

    Add Task_30

---
+++ Seminar_4_Functions/Task_30/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Выполнил Кошелев Андрей/ Группа GU | Разработчик | 2789 |
+Напишите программу, которая выводит массив из 8 элементов, 
+заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]
+*/
+
+int[] FillArray(int n)
+{
+    int[] Array = new int[n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Array[i] = rnd.Next(0, 2);
+    }
+
+    return Array;
+}
+
+Console.WriteLine("Введите размерность массива : ");
+int size = int.Parse(Console.ReadLine());
+
+int[] Arr = FillArray(size);
+
+for (int i = 0; i < Arr.Length; i++)
+{
+
+    Console.Write(Arr[i] + " ");
+}
