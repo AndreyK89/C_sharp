34f9fa6ec764f0fd03e2bcb669178110f6b2504e
Andrey_K <kaa89job@yandex.ru>
2022-08-03 01:17:23 +0500

    Add Task_28

---
+++ Seminar_4_Functions/Task_28/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Выполнил Кошелев Андрей/ Группа GU | Разработчик | 2789 |
+Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24
+5 -> 120
+*/
+
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Multiply(a));
+
+
+
+int Multiply(int n)
+{
+    int res = 1;
+    for (int i = 1; i <= a; i++)
+    {
+        res = res * i;
+    }
+    return res;
+}
