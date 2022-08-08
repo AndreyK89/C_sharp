b9a051c601a52710a3ebfaaf1d60c76ef8b190a2
Andrey_K <kaa89job@yandex.ru>
2022-08-09 01:12:23 +0500

    Add Task_34

--- Seminars_05_Practical_Task/Task_34/Program.cs
+++ Seminars_05_Practical_Task/Task_34/Program.cs
@@ -4,8 +4,10 @@
 [345, 897, 568, 234] -> 2
 */
 
-Console.WriteLine($"Массив случайных трёхзначных чисел: ");
+
 int[] numbers = new int[7];
+Console.WriteLine($"Массив случайных трёхзначных чисел: ");
+Console.Write("[");
 FillPrintArray(numbers);
 Console.WriteLine();
 SumPositive(numbers);
@@ -26,7 +28,14 @@ void FillPrintArray(int[] array)
     for (int i = 0; i < array.Length; i++)
     {
         array[i] = new Random().Next(100, 1000);
-        Console.Write($"{array[i]} ");
+        Console.Write($"{array[i]}");
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+        else
+        {
+            Console.Write("]");
+        }
     }
-    
 }
---
+++ Seminars_05_Practical_Task/Task_36/Program.cs
@@ -0,0 +1,42 @@
+﻿/* Выполнил Кошелев Андрей/ Группа GU | Разработчик | 2789
+Задача 36: Задайте одномерный массив, заполненный случайными числами.
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+
+
+int[] numbers = new int[7];
+Console.WriteLine($"Массив случайных чисел: ");
+Console.Write("[");
+FillPrintArray(numbers);
+
+
+int sum = 0;
+for (int i = 1; i < numbers.Length; i++)
+{
+    sum = numbers[i] + sum;
+    i++;
+}
+
+Console.WriteLine();
+Console.WriteLine($"Cумма элементов, стоящих на нечетных позициях = {sum}");
+
+
+void FillPrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 100);
+        Console.Write($"{array[i]}");
+      if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+        else
+        {
+            Console.Write("]");
+        }
+    }
+}
