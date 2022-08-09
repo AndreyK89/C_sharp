5a729f9560f913e7fbce903e65dd52db5f9e7a46
Andrey_K <kaa89job@yandex.ru>
2022-08-09 23:43:54 +0500

    Add Task_FizzBass_prepodavatel

--- Seminar_6_Recursion/Task_FizzBass_prepodavatel/Program.cs
+++ Seminar_6_Recursion/Task_FizzBass_prepodavatel/Program.cs
@@ -5,3 +5,23 @@
 */
 
 
+for (int i = 1; i < 101; i++)
+{
+    
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        System.Console.WriteLine("FizzBuzz");
+    }
+   else if (i % 5 == 0)
+    {
+        System.Console.WriteLine("Buzz");
+    }
+    else if (i % 3 == 0)
+    {
+        System.Console.WriteLine("Fizz");
+    }
+    else 
+    {
+        System.Console.WriteLine(i);
+    }
+}
