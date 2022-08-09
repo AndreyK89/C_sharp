70fb72cd2ab725158df7030641a843885d5e4517
Andrey_K <kaa89job@yandex.ru>
2022-08-10 00:12:52 +0500

    Add Task 42

--- Seminar_6_Recursion/Task_42/Program.cs
+++ Seminar_6_Recursion/Task_42/Program.cs
@@ -6,3 +6,17 @@
 */
 
 
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int[] res = new int[32];
+int i = 0;
+while(n > 0)
+{
+    res[i] = n % 2;
+    i++;
+    n = n / 2;
+}
+for(int j = i - 1; j >= 0; j--)
+{
+    Console.Write(res[j]);
+}
