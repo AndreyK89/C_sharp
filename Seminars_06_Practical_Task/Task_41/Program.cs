24e7eddd07fd0b687ca8337e4ebf09f7be8af319
Andrey_K <kaa89job@yandex.ru>
2022-08-12 01:55:20 +0500

    Add Task 43

---
+++ Seminars_06_Practical_Task/Task_41/Program.cs
@@ -0,0 +1,78 @@
+﻿/* Выполнил Кошелев Андрей/ Группа GU | Разработчик | 2789
+Задача 41: Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+
+
+
+Console.Write("Введите кол-во чисел для ввода: ");
+int length = int.Parse(Console.ReadLine());
+int[] array = new int[length];
+
+FillArry(array);
+WriteArray(array);
+Positive(array);
+
+
+
+void Positive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+        else count += 0;
+    }
+    Console.Write($"Чисел больше 0 введено: {count}");
+}
+
+void FillArry(int[] array)
+{
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"Введите любое число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void WriteArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+ */
+
+Console.Clear();
+
+Console.WriteLine($"Задача 41. Cколько чисел больше 0 ввёл пользователь \n");
+Console.Write($"Введи число М(количество чисел): ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] massiveNumbers = new int[m];
+
+void InputNumbers(int m)
+{
+    for (int i = 0; i < m; i++)
+    {
+        Console.Write($"Введи {i + 1} число: ");
+        massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+
+int Comparison(int[] massiveNumbers)
+{
+    int count = 0;
+    for (int i = 0; i < massiveNumbers.Length; i++)
+    {
+        if (massiveNumbers[i] > 0) count += 1;
+    }
+    return count;
+}
+
+InputNumbers(m);
+
+Console.WriteLine($"Введено чисел больше 0: {Comparison(massiveNumbers)} ");
---
+++ Seminars_06_Practical_Task/Task_43/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Выполнил Кошелев Андрей/ Группа GU | Разработчик | 2789.
+ Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых,
+ заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+ значения b1, k1, b2 и k2 задаются пользователем.    в уравнениях => y = k1 * x + b1, y = k2 * x + b2
+ b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 0,5)
+*/
+
+Console.WriteLine();
+Console.WriteLine("Задача 43:");
+Console.WriteLine("Напишите программу, которая найдёт точку пересечения двух прямых,");
+Console.WriteLine("заданных уравнениями:");
+Console.WriteLine("y = k1 * x + b1; y = k2 * x + b2.");
+Console.WriteLine("Введите значения:");
+
+double b1 = SetValues("b1");
+double k1 = SetValues("k1");
+double b2 = SetValues("b2");
+double k2 = SetValues("k2");
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * ((b2 - b1) / (k1 - k2)) + b1;
+
+Console.Write($"Точка пересечения двух прямых: x({x}); y({y}). ");
+
+double SetValues(string value)
+{
+    Console.Write($"{value}: ");
+    double number = Convert.ToDouble(Console.ReadLine());
+    return number;
+}
+
