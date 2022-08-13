6ea0b63b8e3965ae38a53ceb4c5583d976e2d466
Andrey_K <kaa89job@yandex.ru>
2022-08-14 00:14:09 +0500

    Add Task_47

---
+++ Seminars_07_Practical_Task/Task_47/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Выполнил Кошелев Андрей/ Группа GU | Разработчик | 2789 |
+Задача 47: Задайте двумерный массив размером m×n,
+заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+
+Console.WriteLine("Введите количество строк и столбцов через пробел: ");
+string[] input = Console.ReadLine().Split();
+int rows = int.Parse(input[0]);
+int columns = int.Parse(input[1]);
+double[,] numbers = new double[rows, columns];
+RandomArray(numbers);
+PrintArray(numbers);
+
+void RandomArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write(" | ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " | ");
+        }
+        Console.WriteLine("");
+    }
+}
