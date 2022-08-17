30e0e5f12b077f520ff4249117bc9d0f4cf8cb1f
Andrey_K <kaa89job@yandex.ru>
2022-08-17 16:51:13 +0500

    Task_47

--- Seminars_07_Practical_Task/Task_47/Program.cs
+++ Seminars_07_Practical_Task/Task_47/Program.cs
@@ -31,10 +31,11 @@ void PrintArray(double[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
+        //Console.Write(" | ");
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.Write(array[i, j] + "\t ");
+            Console.Write(array[i, j] + "\t ");   // Console.Write(array[i, j] + " | ");
         }
-        Console.WriteLine(" \t  ");
+        Console.WriteLine(" \t  ");  // Console.WriteLine("  ");
     }
 }
