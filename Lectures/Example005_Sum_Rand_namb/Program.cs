49afd7b28e121b7df028b8800913260aafa5e172
Andrey_K <kaa89job@yandex.ru>
2022-08-18 12:08:23 +0500

    Example005 Sum Rand namb

--- Lectures/Example005_Sum_Rand_namb/Program.cs
+++ Lectures/Example005_Sum_Rand_namb/Program.cs
@@ -1,73 +1,9 @@
 ﻿// Сумма случайных чисел.
-//Console.WriteLine("Hello, World!");
-//int numberA = new Random().Next(1, 10); // 123…9
-//Console.WriteLine(numberA);
-//int numberB = new Random().Next (1, 10); // 123...9
-//Console.WriteLine(numberB);
-//int result = numberA + numberB;
-//Console.WriteLine(result);
+Console.WriteLine("Hello, World!");
+int numberA = new Random().Next(1, 10); // 123…9
+Console.WriteLine(numberA);
+int numberB = new Random().Next (1, 10); // 123...9
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
 
-
-int InputExamNumber() // ввод и проверка числа
-{
-    while (true)
-    {
-        string text = Console.ReadLine();
-        if (int.TryParse(text, out int number))
-        {
-            return number;
-            break;
-        }
-        Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
-    }
-}
-
-void PrintArray(int[,] matr)
-{
-    for (int i = 0; i < matr.GetLength(0); i++)
-    {
-        for (int j = 0; j < matr.GetLength(1); j++)
-        {
-            Console.Write($" {matr[i, j]}");
-        }
-        Console.WriteLine();
-    }
-}
-
-void FillArray(int[,] matr)
-{
-    for (int i = 0; i < matr.GetLength(0); i++)
-    {
-        for (int j = 0; j < matr.GetLength(1); j++)
-        {
-            matr[i, j] = new Random().Next(1, 10);
-        }
-    }
-}
-void FindNumber(int[,] matr, int value)
-{
-    int count = 0;
-    for (int i = 0; i < matr.GetLength(0); i++)
-    {
-        for (int j = 0; j < matr.GetLength(1); j++)
-        {
-            if (matr[i, j] == value)
-            {
-                Console.WriteLine($"{count + 1} позиция числа: строка {i+1} и столбец {j+1}");
-                count++;
-            }
-        }
-    }
-    if (count == 0) Console.WriteLine("Такого элемента нет");
-}
-
-Console.WriteLine("Введите количество строк");
-int m = InputExamNumber();
-Console.WriteLine("Введите количество столбцов");
-int n = InputExamNumber();
-int[,] array = new int[m, n];
-FillArray(array);
-PrintArray(array);
-Console.WriteLine("Введите число");
-int x = InputExamNumber();
-FindNumber(array, x);
