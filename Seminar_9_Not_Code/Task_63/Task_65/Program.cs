9b1d54c7a7559dd3f53221044c81c805841fca57
Andrey_K <kaa89job@yandex.ru>
2022-08-19 23:10:43 +0500

    Task_65

---
+++ Seminar_9_Not_Code/Task_63/Task_65/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задача 65: Задайте значение N. Напишите функцию,
+которая выведет все натуральные числа в промежутке от N до 0.
+M = 1; N = 5. -> "1, 2, 3, 4, 5"
+M = 4; N = 8. -> "4, 5, 6, 7, 8"
+*/
+
+/*
+Console.WriteLine("Введите число N ");
+int n = int.Parse(Console.ReadLine());
+Naturalback(n);
+
+void Naturalback(int start)
+{
+    if (start < 0)
+    {
+        return;
+    }
+    Console.Write(start + "\t");
+    start--;
+    Naturalback(start);
+}
+
+*/
+
+Console.WriteLine("Введите число N ");
+int n = int.Parse(Console.ReadLine());
+//Console.WriteLine(--n);
+Console.Write(Naturalback(n, " "));
+
+string Naturalback(int start, string res)
+{
+    if (start < 0)
+    {
+        return res;
+    }
+    //Console.Write(start + "\t"+ Naturalback(start--));
+    return ($"{start}  \t {Naturalback(--start, res)} ");
+ }
